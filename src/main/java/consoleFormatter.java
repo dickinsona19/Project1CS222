754448d2616fe97e95b67bbdc4c0c6f7ebacdbd6
dicki <dickinsona19@yahoo.com>
2021-02-22 06:51:57 -0500

    Code compiles, everything but "doesWikiArticleExist" works

---
+++ src/main/java/consoleFormatter.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class consoleFormatter {
+    public void printTimestampUser(ArrayList<String> parsedJson) {
+        timeStampAndUserConfiguration TSAUConfig = new timeStampAndUserConfiguration();
+
+
+        ArrayList<String> users = (TSAUConfig.configureUser(parsedJson));
+        ArrayList<String> timestamps = (TSAUConfig.configureTimestamp(parsedJson));
+
+        for (int i = 0; i < timestamps.size() || i > 30; i++) {
+            System.out.println(timestamps.get(i)+ "  " + users.get(i));
+        }
+    }
+}
